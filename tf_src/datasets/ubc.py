ee8ab7d80b4411e61c7997c56a97198dfe22e887
edgarriba <edgar.riba@gmail.com>
2017-02-03 11:10:17 +0100

    refactor ubc dataset

--- tf_src/datasets/ubc.py
+++ tf_src/datasets/ubc.py
@@ -1,5 +1,6 @@
 import os
 import cv2
+
 import numpy as np
 
 from tqdm import tqdm
@@ -8,12 +9,8 @@ from tqdm import tqdm
 class UBCDataset(object):
     # the extension of the images containing the patches
     IMAGE_EXT = 'bmp'
-    # the size of the patches once extracted
-    PATCH_SIZE = 64
-    # the number of patches per row/column in the
-    # image containing all the patches
-    PATCHES_PER_ROW = 16
-    PATCHES_PER_IMAGE = PATCHES_PER_ROW**2
+    # the file containing the matches
+    matches_filename = 'm50_100000_100000_0.txt'
 
     def __init__(self, base_dir, test=False):
         # check that the directories exist
@@ -33,9 +30,6 @@ class UBCDataset(object):
             'notredame': None,
             'yosemite':  None
         }
-
-        # the file containing the matches
-        self.matches_filename = 'm50_100000_100000_0.txt'
 
     def load_by_name(self, name, patch_size=32, num_channels=1, debug=True):
         assert name in self._data.keys(), \
@@ -92,7 +86,7 @@ class UBCDataset(object):
                 matches.append([int(l[0]), int(l[3]), int(l[1] == l[4])])
         return np.asarray(matches)
 
-    def _load_patches(self, name, patch_size, num_channels):
+    def _load_patches(self, name, img_sz, n_channels):
         """
         Return a list containing all the patches
         """
@@ -108,36 +102,36 @@ class UBCDataset(object):
                     files.append(os.path.join(dataset_dir, file_name))
             return sorted(files)  # sort files in ascend order to keep relations
 
-        patches_all = []
-
-        # load the images containing the patches
-        img_files = load_image_filenames(self._base_dir, name)
+        images = []
+        list_files = load_image_filenames(self._base_dir, name)
         # reduce the number of files to load if we are in testing mode
-        img_files = img_files [0:self.n] if self.test else img_files
+        list_files = list_files[0:self.n] if self.test else list_files
+
         # load patches
-        pbar = tqdm(img_files)
-        for file in pbar:
+        pbar = tqdm(list_files)
+        for file_name in pbar:
             pbar.set_description('Loading dataset %s' % name)
             # pick file name
-            assert os.path.isfile(file), 'Not a file: %s' % file
+            assert os.path.isfile(file_name), 'Not a file: %s' % file
             # load the image containing the patches and convert to float point
             # and make sure that que use only one single channel
-            img = cv2.imread(file)[:, :, 0] / 255.
+            img = cv2.imread(file_name, cv2.IMREAD_GRAYSCALE)
             # split the image into patches and
             # add patches to buffer as individual elements
-            patches_row = np.split(img, self.PATCHES_PER_ROW, axis=0)
+            patches_row = np.split(img, 16, axis=0)
             for row in patches_row:
-                patches = np.split(row, self.PATCHES_PER_ROW, axis=1)
+                patches = np.split(row, 16, axis=1)
                 for patch in patches:
                     # resize the patch
-                    patch_resize = cv2.resize(patch, (patch_size,patch_size))
+                    patch_resize = cv2.resize(patch, (img_sz, img_sz),
+                                              cv2.INTER_LINEAR)
                     # convert to tensor [w x h x d]
-                    patch_tensor = patch_resize.reshape(patch_size,
-                                                        patch_size,
-                                                        num_channels)
-                    patches_all.append(patch_tensor)
-        return np.asarray(patches_all) if not self.test \
-            else np.asarray(patches_all[0:self.n])
+                    patch_tensor = patch_resize.reshape(img_sz,
+                                                        img_sz,
+                                                        n_channels)
+                    images.append(patch_tensor / 255.)
+        return np.array(images) if not self.test \
+            else np.asarray(images[0:self.n])
 
     def _load_labels(self, base_dir, dir_name):
         """
